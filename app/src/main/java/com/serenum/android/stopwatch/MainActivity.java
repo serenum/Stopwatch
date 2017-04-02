028bf7f9d3a06383b1a8d0a991a22823a497149e
serenum <saveliev_@rambler.ru>
2017-04-02 21:58:29 +0600

    First version of the app.

--- app/src/main/java/com/serenum/android/stopwatch/MainActivity.java
+++ app/src/main/java/com/serenum/android/stopwatch/MainActivity.java
@@ -1,13 +1,53 @@
 package com.serenum.android.stopwatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int seconds;
+    public boolean running;
+    public boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView) findViewById(R.id.stopwatch_display);
+        final Handler secondPeriod = new Handler();
+        secondPeriod.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int min = (seconds%3600)/60;
+                int sec = seconds%60;
+                String time = String.format("%d:%02d:%02d", hours, min, sec);
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                secondPeriod.postDelayed(this, 1000);
+            }
+        });
     }
 }
